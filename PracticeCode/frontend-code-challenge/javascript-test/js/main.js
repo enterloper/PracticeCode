b82996c063b538f747b7e86c9405919b7065cd15
Richard Boothe <richardjboothe@me.com>
2016-10-08 20:59:06 -0500

    add sorting function

--- PracticeCode/frontend-code-challenge/javascript-test/js/main.js
+++ PracticeCode/frontend-code-challenge/javascript-test/js/main.js
@@ -1,8 +1,3 @@
 var standard = Object.create(publicAPI.NewDeck);
 standard.deck = standard.orderDeck()
 standard.shuffleDeck(standard.deck)
-var standard1 = Object.create(publicAPI.NewDeck);
-standard1.deck = standard.orderDeck()
-standard1.shuffleDeck(standard1.deck)
-// console.log(standard.deck)
-// console.log(standard1.deck)
