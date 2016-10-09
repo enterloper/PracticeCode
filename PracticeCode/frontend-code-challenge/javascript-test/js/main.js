b3791735e7fa2345389ba76310aa636bde4f54bf
Richard Boothe <richardjboothe@me.com>
2016-10-08 23:00:03 -0500

    add num input

--- PracticeCode/frontend-code-challenge/javascript-test/js/main.js
+++ PracticeCode/frontend-code-challenge/javascript-test/js/main.js
@@ -1,8 +1,17 @@
+var standard = Object.create(publicAPI.NewDeck);
+standard.deck = standard.orderDeck();
+standard.shuffleDeck(standard.deck);
+var count = standard.deck.length;
+console.log('---->first',standard.deck, count)
+$(function() {
+  $('.leftInDeck').text(count);
+});
 
-
-$(function(){
-  var standard = Object.create(publicAPI.NewDeck);
-  standard.deck = standard.orderDeck()
+$("#shuffleButton").click(function() {
   standard.shuffleDeck(standard.deck)
-  console.log(standard.deck)
+  console.log('Hey no cheating!---->',standard.deck)
+})
+
+$("#cutButton").click(function() {
+  $('numSelect').removeClass('show')
 })
