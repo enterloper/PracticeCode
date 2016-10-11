46c1ea5fbd6d42388cc3c3fb3eab65bf15e5c007
Richard Boothe <richardjboothe@me.com>
2016-10-10 22:54:45 -0500

    add submit input

--- PracticeCode/frontend-code-challenge/javascript-test/js/main.js
+++ PracticeCode/frontend-code-challenge/javascript-test/js/main.js
@@ -1,17 +1,19 @@
-var standard = Object.create(publicAPI.NewDeck);
-standard.deck = standard.orderDeck();
-standard.shuffleDeck(standard.deck);
-var count = standard.deck.length;
-console.log('---->first',standard.deck, count)
-$(function() {
-  $('.leftInDeck').text(count);
-});
+// var standard = Object.create(publicAPI.NewDeck);
+// standard.deck = standard.orderDeck();
+// standard.shuffleDeck(standard.deck);
+// var count = standard.deck.length;
+// console.log('---->first',standard.deck, count)
+// $(function() {
+//   $('.leftInDeck').text(count);
+// });
 
-$("#shuffleButton").click(function() {
-  standard.shuffleDeck(standard.deck)
-  console.log('Hey no cheating!---->',standard.deck)
-})
+// $("#shuffleButton").click(function() {
+//   standard.shuffleDeck(standard.deck)
+//   console.log('Hey no cheating!---->',standard.deck)
+// })
 
-$("#cutButton").click(function() {
-  $('numSelect').removeClass('show')
-})
+// $("#cutButton").click(function() {
+//   $('numSelect').removeClass('show')
+// })
+
+// document.body.addEventListener('load', document.write('hello'));
